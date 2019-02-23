aa9668f0bab4e605dfad2f968648ff318d1239db
mromaniv <ext-maksym.romaniv@here.com>
2019-02-23 14:48:14 +0200

    [reactjs] added basic module

--- front-end-advanced/project/index.js
+++ front-end-advanced/project/index.js
@@ -1,6 +1,22 @@
-import React from 'react';
+// game
 import {Game} from "./src/game";
 
 const game = Game.getInstance();
 
 window.game = game;
+
+// react
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+let HelloWorld = () => {
+    return <h1>Hello there World!</h1>
+};
+
+ReactDOM.render(
+    <HelloWorld/>,
+    document.getElementById("root")
+);
+
--- front-end-advanced/project/webpack.config.js
+++ front-end-advanced/project/webpack.config.js
@@ -2,17 +2,35 @@ var path    = require('path');
 var hwp     = require('html-webpack-plugin');
 
 module.exports = {
+    mode: process.env.NODE_ENV || 'development',
     entry: path.join(__dirname, './index.js'),
     output: {
         filename: 'build.js',
         path: path.join(__dirname, '/dist')
     },
-    module:{
-        rules:[{
-            exclude: /node_modules/,
-            test: /\.js$/,
-            loader: 'babel-loader'
-        }]
+    module: {
+        rules:[
+            {
+                // this is so that we can compile any React,
+                // ES6 and above into normal ES5 syntax
+                test: /\.(js|jsx)$/,
+                // we do not want anything from node_modules to be compiled
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                loaders: ['file-loader']
+            }
+        ]
     },
     plugins:[
         new hwp({template:path.join(__dirname, './index.html')})
